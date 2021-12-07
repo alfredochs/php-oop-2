e06458d4c4601f154bf234a43f94e9019cb14d35
alfredochs <chanta.alfredo@gmail.com>
2021-12-07 17:47:20 +0100

    prova push

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente
+dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium 
+che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Provate a far interagire tra di loro gli oggetti: ad esempio, l’utente dello shop inserisce una carta di credito...
+$c = new CreditCard(..);
+$user->insertCreditCard($c);
+BONUS:
+Gestite eventuali eccezioni che si possono verificare (es: carta di credito scaduta)  -->
+
+<?php
+
+class Products
+{
+    protected $name;
+    protected $price;
+    protected $description;
+
+    public function __construct($name, $price, $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->prica = $description;
+    }
+}
